d3b2a724114345e14edd10f25c82a149e1acd996
auxfuse <aux.fuse@yahoo.com>
2019-12-15 15:48:01 +0000

    Testing conneection to MongoDB

--- app.py
+++ app.py
@@ -6,21 +6,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+app = Flask(__name__)
 MONGO_DBNAME = os.getenv("MONGO_DBNAME")
 MONGODB_URI = os.getenv("MONGO_URI")
 
-app = Flask(__name__)
 
+mongo = PyMongo(app)
 
 @app.route('/')
 
 
-@app.route('/base')
-def test():
-    return render_template('base.html')
+@app.route('/fundamentals')
+def show_fundamentals():
+    return render_template('fundamentals.html', fundamentals=mongo.db.fundamental_movements.find())
 
 
 if __name__ == '__main__':
-    app.run(host = os.environ.get('IP'),
-            port = os.environ.get('PORT'),
+    app.run(host=os.environ.get('IP'),
+            port=os.environ.get('PORT'),
             debug=True)
