2e6ff616d92a669f4757ceee79c1fd4453965953
auxfuse <aux.fuse@yahoo.com>
2019-12-15 00:06:24 +0000

    Database connection string and collection name placed in .env file, .env ignored to ensure security of same

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectID
+from bson.objectid import ObjectId
+from dotenv import load_dotenv
 
+load_dotenv()
+
+MONGO_DBNAME = os.getenv("MONGO_DBNAME")
+MONGODB_URI = os.getenv("MONGO_URI")
 
 app = Flask(__name__)
 
