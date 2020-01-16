f502a528f8fec0b5481c68cbdda8cc62b4bccca9
auxfuse <aux.fuse@yahoo.com>
2020-01-16 23:34:49 +0000

    Login/Register Modal content placed and styled

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 from os import path
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if path.exists("env.py"):
     import env
 
