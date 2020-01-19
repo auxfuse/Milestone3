cda1c85f6c412bcd6f3986da97f1041d89622741
auxfuse <aux.fuse@yahoo.com>
2020-01-19 22:09:01 +0000

    class RegisterForm created in forms.py to handle register form generation

--- forms.py
+++ forms.py
@@ -1,9 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = StringField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=5, max=15)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=5, max=12)])
+    password_check = PasswordField('Confirm Password', validators=[DataRequired(),
+                                                                   EqualTo('password', message='Passwords must be the same.')])
+    submit = SubmitField('Register')
