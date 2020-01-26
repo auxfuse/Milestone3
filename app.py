5e0ac7213910ba9b3f72a7febd49f305dc8c07a4
auxfuse <aux.fuse@yahoo.com>
2020-01-26 15:58:50 +0000

    debug=False to test the 500 custom error page

--- app.py
+++ app.py
@@ -203,4 +203,4 @@ def response_500(exception):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '127.0.0.1'),
             port=os.environ.get('PORT', '5000'),
-            debug=True)
+            debug=False)
